99ea4244e81fe95574105da4905c59842d112154
pervin agayev <pervinagayev28@gmail.com>
2023-10-16 17:46:51 +0400

    TelegramPage

---
+++ Telegram/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Telegram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void window_doubleclick(object sender, MouseButtonEventArgs e)
+        {
+            if (WindowState != WindowState.Maximized)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+
+
+        }
+
+        private void window_mouse_dwon(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
